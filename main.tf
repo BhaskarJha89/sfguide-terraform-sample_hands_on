cc107c5847fc225dfb12be5f51f9f8eed985e513
codeninja <bhaskar4jha@gmail.com>
2022-08-16 19:41:20 +0530

    committed

--- main.tf
+++ main.tf
@@ -8,16 +8,75 @@ terraform {
 }
 
 provider "snowflake" {
+  alias = "sys_admin"
   role  = "SYSADMIN"
 }
 
 resource "snowflake_database" "db" {
+  provider = snowflake.sys_admin
   name     = "TF_DEMO"
 }
 
 resource "snowflake_warehouse" "warehouse" {
+  provider       = snowflake.sys_admin
   name           = "TF_DEMO"
-  warehouse_size = "small"
+  warehouse_size = "X-small"
 
   auto_suspend = 60
+}
+
+provider "snowflake" {
+  alias = "security_admin"
+  role  = "SECURITYADMIN"
+}
+
+resource "snowflake_role" "role" {
+  provider = snowflake.security_admin
+  name     = "TF_DEMO_SVC_ROLE"
+}
+
+resource "snowflake_database_grant" "grant" {
+  provider          = snowflake.security_admin
+  database_name     = snowflake_database.db.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+
+resource "snowflake_schema" "schema" {
+  provider   = snowflake.sys_admin
+  database   = snowflake_database.db.name
+  name       = "TF_DEMO"
+  is_managed = false
+}
+
+resource "snowflake_schema_grant" "grant" {
+  provider          = snowflake.security_admin
+  database_name     = snowflake_database.db.name
+  schema_name       = snowflake_schema.schema.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+
+resource "snowflake_warehouse_grant" "grant" {
+  provider          = snowflake.security_admin
+  warehouse_name    = snowflake_warehouse.warehouse.name
+  privilege         = "USAGE"
+  roles             = [snowflake_role.role.name]
+  with_grant_option = false
+}
+
+resource "snowflake_user" "user" {
+  provider          = snowflake.security_admin
+  name              = "chandrab@useready.com"
+  default_warehouse = snowflake_warehouse.warehouse.name
+  default_role      = snowflake_role.role.name
+  default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
+}
+
+resource "snowflake_role_grants" "grants" {
+  provider  = snowflake.security_admin
+  role_name = snowflake_role.role.name
+  users     = [snowflake_user.user.name]
 }
